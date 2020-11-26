2810b4f254f375b2259e13e76d2045726f0aebf0
iranimoroni <iranimoroni@gmail.com>
2020-11-26 10:17:04 -0300

    somando

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+import {useState} from 'react';
+
+
 function home() {
-  return <div>Alo Mundo!!</div>
+return (<div>
+  <div><Contador /></div>
+  <Caps>ola mundo - </Caps>xxx
+  </div>
+)
+}
+export default home
+
+
+
+function Caps(props){
+  const textoin = props.children;
+  const textoCap = textoin.toUpperCase();
+return (<div>
+  <div>{textoCap}</div>
+  </div>
+)
 }
 
-export default home
+function Contador(props){
+  const [contador,setContador] = useState(1);
+  function somaContador(){
+    setContador(contador + 1);
+  }
+  return (
+    <div>
+    <button onClick={somaContador}>valendo - {contador}</button>
+    </div>
+  )
+}
+
